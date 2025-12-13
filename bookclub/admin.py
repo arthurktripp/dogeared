9aee3e6dd1f17f69041a98b1f44e3038308849af
arthurktripp <arthur.k.tripp@gmail.com>
2025-12-13 13:06:37 -0800

    models and admin started

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Book, Member, Shelf
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("author", "title")
+
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("username", "first_name", "last_name")
+
+
+@admin.register(Shelf)
+class ShelfAdmin(admin.ModelAdmin):
+    list_display = ("name",)
--- bookclub/models.py
+++ bookclub/models.py
@@ -1,27 +1,29 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Book(models.Model):
     author = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     isbn = models.CharField(max_length=20)
-    
+
     def __str__(self):
         return self.title
 
 
-class Library(models.Model):
-    name = models.CharField(max_length=255)
-    books = models.ManyToManyField(Book, related_name='libraries')
-    
-    def __str__(self):
-        return self.name
-
-
-class User(models.Model):
+class Member(models.Model):
     username = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    libraries = models.OnetoMany(Library, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.username
+
+
+class Shelf(models.Model):
+    # A shelf is a collection of books that belongs to a specific member.
+    name = models.CharField(max_length=255)
+    books = models.ManyToManyField(Book, related_name='shelves')
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
